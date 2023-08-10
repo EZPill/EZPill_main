9116f552f9105018048a695b45c8ad34931936cf
Lee jaehoon <kaak334@naver.com>
2023-08-10 14:32:42 +0900

    first

---
+++ Desktop/web1/src/App.js
@@ -0,0 +1,15 @@
+import "./App.css";
+import React from "react";
+import AuthView from "./views/AuthView";
+
+
+
+
+
+export default function App() {
+  return (
+    <>
+      <AuthView />
+    </>
+  );
+}
